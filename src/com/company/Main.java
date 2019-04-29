0834f4a5920c8d406232b4b21a387cf9e2c20efd
Victor <victorsantnunes@hotmail.com>
2019-04-29 19:44:12 -0300

    #

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String texto = "Dear Sally, " +
+                "Please, please do it--it would please " +
+                "Mary very, very much. And Mary would " +
+                "do everything in Mary's power to make " +
+                "it pay off for you. " +
+                "-- Thank you very much --";
+        System.out.println(compactarArquivo(texto));
+    }
+
+    public static String compactarArquivo(String arquivo) {
+        String[] saida = arquivo.split(" ");
+        String[] compare = new String[saida.length];
+        String[] arqSaida = new String[saida.length];
+        int count = 0;
+        for (int i = 0; i < saida.length; i++) {
+            compare[i] = saida[i];
+        }
+
+        for(int i = 0; i < saida.length; i ++) {
+            for(int j = 0; j < compare.length; j++) {
+                if(saida[i].equals(compare[j])) {
+                    arqSaida[i] = compare[j];
+                }
+            }
+        }
+
+        for(String row : arqSaida) {
+            System.out.println(row);
+        }
+
+        return "";
     }
 }
